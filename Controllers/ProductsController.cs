c975d374968e4cbfb5d70c2dcd3c9c803ea3a19a
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-27 05:06:48 +0200

    Using Dependency Injection in Controllers

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,6 +1,31 @@
-﻿namespace WebApp.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
 {
-    public class ProductsController
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
     {
+
+        private DataContext context;
+        
+        public ProductsController(DataContext ctx)
+        {
+            context = ctx;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return context.Products;
+        }
+
+        [HttpGet("{id}")]
+        public Product? GetProduct([FromServices] ILogger<ProductsController> logger)
+        {
+            logger.LogDebug("GetProduct Action Invoked");
+
+            return context.Products.FirstOrDefault();
+        }
     }
 }
