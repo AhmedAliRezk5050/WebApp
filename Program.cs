e7c2d24e4e7649cfb34539a15e1f30a0b5bef84e
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-28 16:47:17 +0200

    Enable Convention Routing - edits

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ app.UseStaticFiles();
 
 app.MapControllers();
 
-app.MapControllerRoute("Default", "{controller=Home}/{action=Index}/{id?}");
+app.MapDefaultControllerRoute();
 
 var context = app.Services.CreateScope()
                           .ServiceProvider
