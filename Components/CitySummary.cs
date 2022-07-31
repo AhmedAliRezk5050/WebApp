b15c18f60bc00578297ffc714e39b195b88131cd
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-31 19:40:36 +0200

    View Components-Returning HTML Fragments-unencoded HTML

--- Components/CitySummary.cs
+++ Components/CitySummary.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApp.Models;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Html;
 
 namespace WebApp.Components
 {
@@ -14,7 +16,8 @@ namespace WebApp.Components
 
         public IViewComponentResult Invoke()
         {
-            return Content("This is a <h3><i>string</i></h3>");
+            return new HtmlContentViewComponentResult(
+                    new HtmlString("This is a <h3><i>string</i></h3>"));
         }
     }
 }
