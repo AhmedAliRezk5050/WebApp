bce657d1d89f0c180c5d7a639ec14ec509217b18
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-31 22:31:52 +0200

    Tag Helpers-init

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,37 +14,24 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddRazorPages();
 
-builder.Services.AddDistributedMemoryCache();
-
-builder.Services.AddSession(options =>
-{
-    options.Cookie.IsEssential = true;
-});
-
-builder.Services.Configure<RazorPagesOptions>(opts =>
-{
-    opts.Conventions.AddPageRoute("/Index", "/extra/page/{id:long?}");
-});
-
 builder.Services.AddSingleton<CitiesData>();
 
 var app = builder.Build();
 
 app.UseStaticFiles();
 
-app.UseSession(); 
-
 app.MapControllers();
 
 app.MapDefaultControllerRoute();
 
-app.MapRazorPages().Add(b => ((RouteEndpointBuilder)b).Order = 2); ;
+app.MapRazorPages();
 
-var context = app.Services.CreateScope()
-                          .ServiceProvider
-                          .GetRequiredService<DataContext>();
+var context = app.Services
+                .CreateScope()
+                .ServiceProvider
+                .GetRequiredService<DataContext>();
 
 // temporary disable
-//SeedData.SeedDatabase(context);
+SeedData.SeedDatabase(context);
 
 app.Run();
