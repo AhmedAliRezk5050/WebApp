5565650dc8ec7bd858f5b4fa896190095071a516
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-08-09 08:45:46 +0200

    Filters - creating and applying an Action Filter

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,15 +8,21 @@ namespace WebApp.Controllers
     {
         public IActionResult Index()
         {
-                return View(
-               "Message",
-               "This is the Index action on the Home controller");
+            return View(
+           "Message",
+           "This is the Index action on the Home controller");
         }
 
         public IActionResult Secure()
         {
-                return View("Message",
-                "This is the Secure action on the Home controller");
+            return View("Message",
+            "This is the Secure action on the Home controller");
+        }
+
+        [ChangeArg]
+        public IActionResult Messages(string message1, string message2 = "None")
+        {
+            return View("Message", $"{message1}, {message2}");
         }
     }
 }
