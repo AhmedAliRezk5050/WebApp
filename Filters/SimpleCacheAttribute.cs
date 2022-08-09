99b4a6ff75a4f733224d2470ba2be7fe726c6e60
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-08-09 07:25:37 +0200

    Filters - applying Asynchronous Resource Filter

--- Filters/SimpleCacheAttribute.cs
+++ Filters/SimpleCacheAttribute.cs
@@ -3,26 +3,33 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace WebApp.Filters
 {
-    public class SimpleCacheAttribute : Attribute, IResourceFilter
+    public class SimpleCacheAttribute : Attribute, IAsyncResourceFilter
     {
         private Dictionary<PathString, IActionResult> CachedResponses
-            = new Dictionary<PathString, IActionResult>();
+                = new Dictionary<PathString, IActionResult>();
 
-        public void OnResourceExecuting(ResourceExecutingContext context)
+        public async Task OnResourceExecutionAsync(
+            ResourceExecutingContext context,
+            ResourceExecutionDelegate next)
         {
             PathString path = context.HttpContext.Request.Path;
 
             if (CachedResponses.ContainsKey(path))
             {
                 context.Result = CachedResponses[path];
+
                 CachedResponses.Remove(path);
             }
-        }
-        public void OnResourceExecuted(ResourceExecutedContext context)
-        {
-            if (context.Result != null)
+            else
             {
-                CachedResponses.Add(context.HttpContext.Request.Path, context.Result);
+                ResourceExecutedContext execContext = await next();
+
+                if (execContext.Result != null)
+                {
+                    CachedResponses.Add(
+                        context.HttpContext.Request.Path,
+                        execContext.Result);
+                }
             }
         }
     }
