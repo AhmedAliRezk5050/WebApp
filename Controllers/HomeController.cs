ef3c434aec9c04826db2c02cc4b23bbd30059e32
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-08-10 05:18:57 +0200

    Filters - Changing Filter Order

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,11 +4,11 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace WebApp.Controllers
 {
-    [Message("This is the controller-scoped filter")]
+    [Message("This is the controller-scoped filter", Order = 10)]
     public class HomeController : Controller
     {
-        [Message("This is the first action-scoped filter")]
-        [Message("This is the second action-scoped filter")]
+        [Message("This is the first action-scoped filter", Order = 1)]
+        [Message("This is the second action-scoped filter", Order = -1)]
         public IActionResult Index()
         {
             return View(
