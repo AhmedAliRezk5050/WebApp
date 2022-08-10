4a0bf2cf434d5482118a4eb5506390001e0da1d7
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-08-10 05:39:35 +0200

    Creating Form Applications - preparations

--- Program.cs
+++ Program.cs
@@ -1,12 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
 using WebApp.Filters;
-using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<DataContext>(opts =>
-{
+builder.Services.AddDbContext<DataContext>(opts => {
     opts.UseSqlServer(builder.Configuration[
     "ConnectionStrings:ProductConnection"]);
     opts.EnableSensitiveDataLogging(true);
@@ -18,23 +16,23 @@ builder.Services.AddRazorPages();
 
 builder.Services.AddScoped<GuidResponseAttribute>();
 
-builder.Services.Configure<MvcOptions>(opts => {
-    opts.Filters.Add<HttpsOnlyAttribute>();
-    opts.Filters.Add(new MessageAttribute("This is the globally-scoped filter"));
-});
-
 var app = builder.Build();
 
 app.UseStaticFiles();
 
+
+app.MapControllers();
+
 app.MapDefaultControllerRoute();
+
+app.MapControllerRoute("forms", "controllers/{controller=Home}/{action=Index}/{id?}");
 
 app.MapRazorPages();
 
 var context = app.Services
-                .CreateScope()
-                .ServiceProvider
-                .GetRequiredService<DataContext>();
+                  .CreateScope()
+                  .ServiceProvider
+                  .GetRequiredService<DataContext>();
 
 SeedData.SeedDatabase(context);
 
