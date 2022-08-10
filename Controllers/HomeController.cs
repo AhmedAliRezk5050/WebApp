f3d4760592f94487a0fe32635c54b1a043f42815
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-08-10 06:09:27 +0200

    Creating Form Applications - use ViewModelFactory

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,5 +24,18 @@ namespace WebApp.Controllers
                                .Include(p => p.Category)
                                .Include(p => p.Supplier));
         }
+
+        public async Task<IActionResult> Details(long id)
+        {
+            Product? p = await context.Products
+                                      .Include(p => p.Category)
+                                      .Include(p => p.Supplier)
+                                      .FirstOrDefaultAsync(p => p.ProductId == id)
+                                        ?? new Product();
+
+            ProductViewModel model = ViewModelFactory.Details(p);
+            
+            return View("ProductEditor", model);
+        }
     }
 }
