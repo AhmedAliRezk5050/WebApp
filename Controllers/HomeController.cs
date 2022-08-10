25f2f637a236ac421b24ec132dca4dc19d298ab4
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-08-10 06:26:26 +0200

    Creating Form Applications - creating data

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -34,8 +34,40 @@ namespace WebApp.Controllers
                                         ?? new Product();
 
             ProductViewModel model = ViewModelFactory.Details(p);
-            
+
             return View("ProductEditor", model);
+        }
+
+        public IActionResult Create()
+        {
+            return View(
+                "ProductEditor",
+                ViewModelFactory.Create(new Product(), Categories, Suppliers));
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                product.ProductId = default;
+
+                product.Category = default;
+
+                product.Supplier = default;
+
+                context.Products.Add(product);
+
+                await context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+
+            }
+
+            return View(
+                "ProductEditor",
+                ViewModelFactory.Create(product, Categories, Suppliers));
         }
     }
 }
